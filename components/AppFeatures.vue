9333a0551c2c9c04ba82b585665b49834b212c4b
lansolo99 <lansolo99@hotmail.fr>
2020-03-21 14:23:26 +0100

    Implement proper features items

--- components/AppFeatures.vue
+++ components/AppFeatures.vue
@@ -3,60 +3,29 @@
     <v-container class="pt-0">
       <v-layout wrap>
         <v-flex
+          v-for="(feature, index) in features"
+          :key="index"
           sm12
           md4
-          class="appFeatures__column"
+          class="appFeatures__column px-3"
           :class="{
-            'py-4 px-5': $vuetify.breakpoint.smAndDown,
-            'py-2 px-5': $vuetify.breakpoint.mdAndUp
+            'py-4': $vuetify.breakpoint.smAndDown,
+            'py-2': $vuetify.breakpoint.mdAndUp
           }"
         >
-          <img
-            src="~assets/images/appFeatures_habits.svg"
-            alt="Create your habits"
-          />
-          <h1 class="">Create your habits</h1>
+          <div class="appFeatures__imgWrapper">
+            <img
+              :src="require(`~/assets/images/feature_${index + 1}.jpg`)"
+              :srcset="
+                `${require(`~/assets/images/feature_${index + 1}@2x.jpg`)} 2x`
+              "
+              :alt="feature.title"
+            />
+          </div>
+
+          <h1 class="appFeatures__title">{{ feature.title }}</h1>
           <p class="appFeatures__column-description">
-            Set tasks, define a schedule among weekly options (everydays, x
-            times a week, specific days or single tasks).
-          </p>
-        </v-flex>
-        <v-flex
-          sm12
-          md4
-          class="appFeatures__column"
-          :class="{
-            'py-4 px-5': $vuetify.breakpoint.smAndDown,
-            'py-2 px-5': $vuetify.breakpoint.mdAndUp
-          }"
-        >
-          <img
-            src="~assets/images/appFeatures_progression.svg"
-            alt="Track your progression"
-          />
-          <h1 class="">Track your progression</h1>
-          <p class="appFeatures__column-description">
-            Level-up your commitment to 100% on a 10 weeks range. Completion
-            records are also available by-task.
-          </p>
-        </v-flex>
-        <v-flex
-          sm12
-          md4
-          class="appFeatures__column"
-          :class="{
-            'py-4 px-5': $vuetify.breakpoint.smAndDown,
-            'py-2 px-5': $vuetify.breakpoint.mdAndUp
-          }"
-        >
-          <img
-            src="~assets/images/appFeatures_reminder.svg"
-            alt="Set a daily reminder"
-          />
-          <h1 class="">Set a daily reminder</h1>
-          <p class="appFeatures__column-description">
-            Activate notifications for your task(s) due today and choose a time
-            that fit your schedule.
+            {{ feature.description }}
           </p>
         </v-flex>
       </v-layout>
@@ -65,25 +34,51 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      features: [
+        {
+          title: 'Unlock 10 doors',
+          description:
+            'Make your way accross the prison, from your cell to the freedom station.'
+        },
+        {
+          title: 'Resolve 13 games',
+          description:
+            'Unlock each door by solving small logic puzzles with increasing difficulty.'
+        },
+        {
+          title: 'EXPLORE THE PRISON',
+          description:
+            'Discover stunning environments in a strange high-tech atmosphere.'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss">
 .appFeatures {
   position: relative;
-  background-color: $color-golden;
+  background-color: $color-anthracite-blue;
   padding-top: 20px;
   padding-bottom: 60px;
   @include textSelection;
+  &__title {
+    text-transform: uppercase;
+  }
 
   &__column {
     text-align: center;
+
     img {
-      width: 70px;
+      width: 100%;
     }
 
     h1 {
-      margin-top: 10px;
+      margin-top: 40px;
       font-size: 25px;
       left: 33px;
       font-weight: 500;
@@ -96,6 +91,13 @@ export default {}
       margin-top: 20px;
       font-weight: 300;
       font-size: 18px;
+    }
+  }
+
+  &__imgWrapper {
+    border-left: 5px solid $color-red;
+    img {
+      display: block;
     }
   }
 }
